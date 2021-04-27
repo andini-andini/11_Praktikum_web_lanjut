4d55ddf2c1e93eaaea867f79c3eb48542797a424
Amelia Widya <aandini072@gmail.com>
2021-04-27 19:55:15 +0700

    create login function in AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use App\Traits\ApiResponse;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
@@ -23,6 +27,24 @@ class AuthController extends Controller
         ]);
 
         $token = $user->createToken('auth_token')->plainTextToken;
+        return $this->apiSuccess([
+            'token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user,
+        ]);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $validated = $request->validate();
+
+        if (!Auth::attempt($validated)) {
+            return $this->apiError('Credentials not match', Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = User::where('email', $validated['email'])->first();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
         return $this->apiSuccess([
             'token' => $token,
             'token_type' => 'Bearer',
